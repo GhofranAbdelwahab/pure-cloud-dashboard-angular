862eb015d697e1bd6a1f5aadaa586b30e7674b75
Ghofran <ghofran.abdelraouf@vodafone.com>
2023-03-27 01:45:37 +0200

    pure-cloud  angular

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -9,11 +9,12 @@ import {DashboardResponse} from "../../models/dashboard.model";
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit, OnDestroy {
-  private dashboard: DashboardResponse[];
+  private dashboard: DashboardResponse[] = [];
 
   constructor(private router: Router, private userServices: DashboardService) {}
 
   ngOnInit() {
+    this.onLogin();
   }
   ngOnDestroy() {
   }
@@ -22,13 +23,6 @@ export class LoginComponent implements OnInit, OnDestroy {
     this.userServices.loadDashboard().subscribe(
       res => {
         this.dashboard = res;
-        this.dashboard.forEach((item)=>{
-          console.log(item.metric);
-          console.log(item.stats.min);
-          console.log(item.stats.max);
-          console.log(item.stats.count);
-          console.log(item.stats.sum);
-        })
       },
       err => {
         alert('Loading Dashboard Failed');
